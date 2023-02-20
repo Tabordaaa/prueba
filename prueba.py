bb819678920cd2c9a60d881e92b90bc1647cf844
Tabordaaa <tabordajuan1214@gmail.com>
2023-02-19 20:49:19 -0500

    dfgegffdgd

---
+++ prueba.py
@@ -0,0 +1,23 @@
+print("Respuestas")
+
+PG = float(input("Digite el numero 1:"))
+PE = float(input("Digite el numero 2:"))
+
+
+PF= PG+PE
+
+print("La suma de los numeros es:", PF)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
